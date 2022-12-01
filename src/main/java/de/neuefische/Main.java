222add18756dffe5e1639d2545fb527ac5765f97
bitbytejoy <bitbytejoy@gmail.com>
2022-12-01 14:48:58 +0100

    init: BIG BANG

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -7,7 +7,19 @@ public class Main {
         NameBook nameBook = new NameBook();
         nameBook.add("Maik");
         Optional<String> maybeName = nameBook.getAt(10);
-        String name = maybeName.get();
+        String name = maybeName.orElse("N/A");
         System.out.println(name);
+
+        boolean containsTest2 = false;
+        String x = null;
+
+        if (x == null) {
+            x = "";
+        }
+
+        containsTest2 = x.contains("Test");
+
+        boolean containsTest = Optional.ofNullable(x).orElse("")
+            .contains("Test");
     }
 }
